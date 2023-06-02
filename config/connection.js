09a4850be0e9d9902dce11acc031d0bc2c2f4025
frankolms <frankolms@gmail.com>
2023-06-01 22:06:48 -0400

    fixed typo in userController.js, started writing thoughtController.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require("mongoose");
 
 const connectionString =
-  proccess.env.MONGODB_URI || "mongodb://127.0.0.1:27017studentsDB";
+  proccess.env.MONGODB_URI || "mongodb://127.0.0.1:27017userssDB";
 connect(connectionString);
 
 module.exports = connection;
--- controllers/userController.js
+++ controllers/userController.js
@@ -17,7 +17,9 @@ module.exports = {
 
   async getSingleUser(req, res) {
     try {
-      const user = await User.findOne({ _id: req.params.userId }).select("__v");
+      const user = await User.findOne({ _id: req.params.userId }).select(
+        "-__v"
+      );
 
       if (!user) {
         return res.status(404).json({ message: "No user with that ID" });
