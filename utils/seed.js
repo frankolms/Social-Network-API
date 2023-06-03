57084da7082b53fc08fca941c33812e67fe6358b
frankolms <frankolms@gmail.com>
2023-06-02 22:43:02 -0400

    finished seed.js

---
+++ utils/seed.js
@@ -0,0 +1,39 @@
+const connection = require("../config/connection");
+const { Thought, User } = require("../models");
+const { getRandomName, getRandomThought } = require("./data");
+
+connection.on("error", (err) => err);
+
+connection.once("open", async () => {
+  console.log("connected");
+
+  await Thought.deleteMany({});
+
+  await User.deleteMany({});
+
+  const users = [];
+  const usernames = [];
+
+  for (let i = 0; i < 20; i++) {
+    const username = getRandomName();
+    usernames.push(username);
+    const email = username.replace(" ", "") + "@email.com";
+
+    const userThought = await Thought.create({
+      thoughtText: getRandomThought(),
+      username,
+    });
+
+    users.push({
+      username,
+      email,
+      thoughts: [userThought._id],
+    });
+  }
+
+  await User.collection.insertMany(users);
+
+  console.table(users);
+  console.info("Seeding complete");
+  process.exit(0);
+});
